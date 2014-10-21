d049774270cf9c6f5dc9f6830717826446b422fa
Jamie Patel <jamie@notespublication.com>
2014-10-21 16:06:32 +0100

    Changed auth

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     // res.setHeader('Access-Control-Allow-Headers', 'Authorization');
     next();
